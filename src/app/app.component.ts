e10bfd41ff537b609661759c5586b62a40dc666a
thulanigamtee <thulanigamtee@gmail.com>
2025-01-20 21:31:00 +0200

    subscribe to overlay service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { OverlayService } from './services/overlay.service';
+import { Subject, takeUntil } from 'rxjs';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio-website';
+  isOverlayActive: boolean = false;
+  private destroy$ = new Subject<void>();
+
+  overlayService = inject(OverlayService);
+
+  ngOnInit() {
+    this.overlayService.overlay$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((state) => (this.isOverlayActive = state));
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
