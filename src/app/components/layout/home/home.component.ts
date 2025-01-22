f92bfcfdf74890c4778b0078b88a0f563e65fa7d
thulanigamtee <thulanigamtee@gmail.com>
2025-01-22 16:47:29 +0200

    create home component

---
+++ src/app/components/layout/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../ui/button/button.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [ButtonComponent],
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {}
