3456ded64cab9808879b8a2e20b281ca6aaf9aaf
thulanigamtee <thulanigamtee@gmail.com>
2025-01-22 16:47:02 +0200

    create logo component

---
+++ src/app/components/shared/logo/logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  imports: [],
+  templateUrl: './logo.component.html',
+})
+export class LogoComponent {
+  name = ['T', 'h', 'u', 'l', 'a', 'n', 'i'];
+}
