11db47ccc4a84a35d237a79e61d549d50ad72974
thulanigamtee <thulanigamtee@gmail.com>
2025-01-22 16:07:05 +0200

    create portfolio component

---
+++ src/app/components/layout/portfolio/portfolio.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [],
+  templateUrl: './portfolio.component.html',
+})
+export class PortfolioComponent {}
