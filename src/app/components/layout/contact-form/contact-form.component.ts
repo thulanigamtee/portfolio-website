8695f56843bd04bdf2b4719d99d415cafb916469
thulanigamtee <thulanigamtee@gmail.com>
2025-01-24 16:54:44 +0200

    update form

--- src/app/components/layout/contact-form/contact-form.component.ts
+++ src/app/components/layout/contact-form/contact-form.component.ts
@@ -7,15 +7,30 @@ import {
   Validators,
 } from '@angular/forms';
 import { ButtonComponent } from '../../ui/button/button.component';
+import { SectionInfoComponent } from '../../shared/section-info/section-info.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [InputComponent, ButtonComponent, ReactiveFormsModule],
+  imports: [
+    InputComponent,
+    ButtonComponent,
+    ReactiveFormsModule,
+    SectionInfoComponent,
+    NgClass,
+  ],
   templateUrl: './contact-form.component.html',
 })
 export class ContactFormComponent {
   contactForm!: FormGroup;
   formBuilder = inject(FormBuilder);
+
+  info: { subTitle: string; title: string; descripition: string } = {
+    subTitle: 'Connect with me',
+    title: 'Get in touch',
+    descripition:
+      "I'd love to hear from you! If you have any questions, comments or feedback, please use the form below.",
+  };
 
   ngOnInit() {
     this.contactForm = this.formBuilder.group({
