a9911354430be6bcf34ca1879cf6edee33bd5667
thulanigamtee <thulanigamtee@gmail.com>
2025-01-24 16:54:18 +0200

    add logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,28 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { OverlayService } from './services/overlay.service';
 import { Subject, takeUntil } from 'rxjs';
 import { NgClass } from '@angular/common';
+import { HomeComponent } from './components/layout/home/home.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { ContactFormComponent } from './components/layout/contact-form/contact-form.component';
+import { PortfolioComponent } from './components/layout/portfolio/portfolio.component';
+import { AboutComponent } from './components/layout/about/about.component';
+import { LogoComponent } from './components/shared/logo/logo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, NgClass],
+  imports: [
+    HeaderComponent,
+    NgClass,
+    HomeComponent,
+    FooterComponent,
+    ContactFormComponent,
+    PortfolioComponent,
+    AboutComponent,
+    LogoComponent,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'portfolio-website';
   isOverlayActive: boolean = false;
   private destroy$ = new Subject<void>();
 
