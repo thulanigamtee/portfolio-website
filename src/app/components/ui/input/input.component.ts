31e4f76110eaad60bacaba8a38f39e5616a5aff6
thulanigamtee <thulanigamtee@gmail.com>
2025-01-24 16:55:57 +0200

    import forms module

--- src/app/components/ui/input/input.component.ts
+++ src/app/components/ui/input/input.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './input.component.html',
 })
-export class InputComponent {}
+export class InputComponent {
+  @Input() label!: string;
+  @Input() type: string = '';
+  @Input() id!: string;
+  @Input() placeholder!: string;
+  @Input() control!: any;
+}
