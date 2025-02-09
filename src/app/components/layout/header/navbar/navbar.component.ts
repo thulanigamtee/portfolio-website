7dd2d8b650eb49f0c95f4aabb86ce5efebf16a35
thulanigamtee <thulanigamtee@gmail.com>
2025-02-09 15:53:00 +0200

    implement scroll to section functionality

--- src/app/components/layout/header/navbar/navbar.component.ts
+++ src/app/components/layout/header/navbar/navbar.component.ts
@@ -1,20 +1,26 @@
 import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../../../ui/button/button.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, NgClass, ButtonComponent],
+  imports: [NgClass, ButtonComponent],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
   navigationLinks: { name: string; path: string }[] = [
-    { name: 'home', path: '' },
-    { name: 'about me', path: '/about' },
-    { name: 'portfolio', path: '/portfolio' },
+    { name: 'home', path: 'home' },
+    { name: 'about me', path: 'about' },
+    { name: 'portfolio', path: 'portfolio' },
     // { name: 'contact me', path: '/contact' },
   ];
 
   @Input() isMobileMenuActive!: boolean;
+
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
