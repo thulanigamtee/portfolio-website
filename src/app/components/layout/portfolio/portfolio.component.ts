750fdbcb1cf75f1d521571cf18424623b6c8b80b
thulanigamtee <thulanigamtee@gmail.com>
2025-03-13 01:40:03 +0200

    update portfolio

--- src/app/components/layout/portfolio/portfolio.component.ts
+++ src/app/components/layout/portfolio/portfolio.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SectionInfoComponent } from '../../shared/section-info/section-info.component';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { NgStyle } from '@angular/common';
 import { Subject, takeUntil } from 'rxjs';
+import { project } from './project.model';
+import { PortfolioService } from '../../../services/portfolio.service';
 
 @Component({
   selector: 'app-portfolio',
@@ -10,6 +12,9 @@ import { Subject, takeUntil } from 'rxjs';
   templateUrl: './portfolio.component.html',
 })
 export class PortfolioComponent {
+  projects: project[] = [];
+  portfolioService = inject(PortfolioService);
+
   info: { subTitle: string; title: string; descripition: string } = {
     subTitle: 'My portfolio',
     title: 'My latest work',
@@ -30,35 +35,8 @@ export class PortfolioComponent {
       .subscribe((state: BreakpointState) => {
         this.isDesktopWidth = state.matches;
       });
+    this.portfolioService.getProjects().then((data) => (this.projects = data));
   }
-
-  projects: {
-    image: string;
-    name: string;
-    description: string;
-    technologies: string;
-    site: string;
-    code: string;
-  }[] = [
-    {
-      image: 'assets/portfolio/audiophile.png',
-      name: 'Audiophile E-commerce Website',
-      description:
-        'A modern, responsive e-commerce website for high-end audio products, built as part of a Frontend Mentor challenge. This project showcases a clean UI, smooth navigation, and a seamless shopping experience for users looking to buy premium headphones, speakers, and earphones.',
-      technologies: 'Angular, TailwindCSS',
-      site: 'https://audiophile-ecommerce-website-mu-ten.vercel.app/',
-      code: 'https://github.com/thulanigamtee/audiophile-ecommerce-website',
-    },
-    {
-      image: '/assets/portfolio/music-app.png',
-      name: 'Full stack music player',
-      description:
-        'A full-stack music streaming application inspired by Spotify, built using Angular, Spring Boot, and PostgreSQL. This project provides a seamless user experience for discovering, playing, and managing music, with a modern UI and robust backend services.',
-      technologies: 'Angular, Spring Boot, PostgreSQL, Bootstrap, 0Auth',
-      site: '',
-      code: 'https://github.com/thulanigamtee/spotify-clone-front',
-    },
-  ];
 
   ngOnDestroy() {
     this.destroy$.next();
