8c67a79258b878dec8a137d7219b43bceea012d0
thulanigamtee <thulanigamtee@gmail.com>
2025-03-14 00:14:39 +0200

    fix environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   appwrite: {
-    endpoint: process.env['APPWRITE_ENDPOINT'] || '',
-    projectId: process.env['APPWRITE_PROJECT_ID'] || '',
-    databaseId: process.env['APPWRITE_DATABASE_ID'] || '',
-    collectionId: process.env['APPWRITE_COLLECTION_ID'] || '',
-    buckedId: process.env['APPWRITE_BUCKET_ID'] || '',
+    endpoint: (window as any)['APPWRITE_ENDPOINT'] || 'http://localhost:3000',
+    projectId: (window as any)['APPWRITE_PROJECT_ID'] || '',
+    databaseId: (window as any)['APPWRITE_DATABASE_ID'] || '',
+    collectionId: (window as any)['APPWRITE_COLLECTION_ID'] || '',
+    buckedId: (window as any)['APPWRITE_BUCKET_ID'] || '',
   },
 };
