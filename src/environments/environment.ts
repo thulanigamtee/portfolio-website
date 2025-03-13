fdabb677ae75296a29dc95e84e5e6e1ed9034a23
thulanigamtee <thulanigamtee@gmail.com>
2025-03-14 01:52:10 +0200

    update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   appwrite: {
-    endpoint: (window as any)['APPWRITE_ENDPOINT'] || 'http://localhost:3000',
-    projectId: (window as any)['APPWRITE_PROJECT_ID'] || '',
-    databaseId: (window as any)['APPWRITE_DATABASE_ID'] || '',
-    collectionId: (window as any)['APPWRITE_COLLECTION_ID'] || '',
-    buckedId: (window as any)['APPWRITE_BUCKET_ID'] || '',
+    endpoint: 'https://cloud.appwrite.io/v1',
+    projectId: '67d17fe0001c21f0d1df',
+    databaseId: '67d1806c001117851d43',
+    collectionId: '67d1807a002ae02f7e8d',
+    buckedId: '67d18266000fcb7656f8',
   },
 };
