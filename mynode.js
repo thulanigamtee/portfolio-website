70b9a7366fc8e786b612ba24d4a811cf59de6650
thulanigamtee <thulanigamtee@gmail.com>
2025-03-15 02:22:11 +0200

    create mynode.js file

---
+++ mynode.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const successColor = "\x1b[32m%s\x1b[0m";
+const checkSign = "\u{2705}";
+const dotenv = require("dotenv").config({ path: "src/.env" });
+
+const envFile = `export const environment = {
+    production: false,
+    appwrite: {
+    endpoint: '${process.env.endpoint}',
+    projectId: '${process.env.projectId}',
+    databaseId: '${process.env.databaseId}',
+    collectionId: '${process.env.collectionId}',
+    buckedId: '${process.env.buckedId}',
+  },
+};
+`;
+const targetPath = path.join(__dirname, "./src/environments/environment.ts");
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(
+      successColor,
+      `${checkSign} Successfully generated environment.ts`
+    );
+  }
+});
